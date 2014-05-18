28cf282272b3eb9c35d31748461a023140280adb
alexchally <alex@alexchally.com>
2014-05-17 21:31:07 -0700

    Threshold, power button and clean up
    I added a lower threshold for the dimming, and turning off the light is
    now done on a button press.

--- prototype_sketch/prototype_sketch.ino
+++ prototype_sketch/prototype_sketch.ino
@@ -7,41 +7,54 @@
 int red_led = 13;
 int blue_led = 12;
 int green_led = 11;
+int power = 52;
+int bias = 5;
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the LEDS.
+  // initialize the LEDs and power switch
   pinMode(red_led, OUTPUT);  
   pinMode(green_led, OUTPUT);
-  pinMode(blue_led, OUTPUT);
-
+  pinMode(blue_led, OUTPUT);  
+  pinMode(power, INPUT);
+  
   //starting a serial connection just to help debug 
   Serial.begin(9600);
 }
 
 void loop() {
-  // read all the analog values for the R G B pots 
-  int red_pot = analogRead(A0);
-  int green_pot = analogRead(A1);
-  int blue_pot = analogRead(A2);
-  int bright_pot = analogRead(A3);
-
+    int power_switch = digitalRead(power);
+    // read all the analog values for the R G B pots 
+    int red_pot = analogRead(A0);
+    int green_pot = analogRead(A1);
+    int blue_pot = analogRead(A2);
+    int bright_pot = analogRead(A3);
+    
+  if (power_switch == 1)
+  {
   // converts the brightness pot to a value from 0 to red255. 
-  float luminosity = bright_pot* (255 /1023.0); // converts the ADC from 0-1023 to 0-255, the proper range for PWM 
+    float luminosity = (bright_pot/1023.0) * 250; // converts the ADC from 0-1023 to 0-250, the proper range for PWM when bias is factored in 
 
-  // adjust the brightness with respect to the luminosity pot
-  float red = (red_pot/1023.0)*(luminosity);
-  float green = (green_pot/1023.0) * (luminosity);
-  float blue = (blue_pot/1023.0) * (luminosity);
+  // adjust the brightness with respect to the luminosity pot. The bias value is to keep the lights from flickering when they get too low.
+    float red = ((red_pot/1023.0) * (luminosity)) + bias;
+    float green = (green_pot/1023.0) * (luminosity) + bias;
+    float blue = (blue_pot/1023.0) * (luminosity) + bias;
 
   //Sets the values for the LEDs
-  analogWrite(red_led, red);   
-  analogWrite(green_led, green);   
-  analogWrite(blue_led, blue);  
-
-
+    analogWrite(red_led, red);   
+    analogWrite(green_led, green);   
+    analogWrite(blue_led, blue); 
+    
+  }
+   else
+    {
+     analogWrite(red_led, 0);   
+     analogWrite(green_led, 0);   
+     analogWrite(blue_led, 0);  
+     
+   }
   // Used for debugging a value:
-  Serial.println(luminosity);
+  Serial.println(power_switch);
   // Serial.println(red_pot);
 
 }
